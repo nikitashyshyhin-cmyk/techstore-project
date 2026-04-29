c3b20773576a00e370c52811b526ba68dfbba167
timosha06 <248711184+timosha06@users.noreply.github.com>
2026-04-29 12:43:53 +0300

    feat(auth): implement password check

--- backend/src/main/java/com/techstore/service/AuthService.java
+++ backend/src/main/java/com/techstore/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.techstore.service;
 
+import com.techstore.dto.LoginRequest;
 import com.techstore.dto.RegisterRequest;
 import com.techstore.entity.User;
 import com.techstore.repository.UserRepository;
+import com.techstore.security.JWTService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +13,13 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
 
     public AuthService(UserRepository userRepository,
-                       PasswordEncoder passwordEncoder) {
+                       PasswordEncoder passwordEncoder, JWTService jwtService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
     public void register(RegisterRequest request) {
@@ -37,5 +41,19 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         userRepository.save(user);
+    }
+
+    public String login(LoginRequest request) {
+        // 1. Знайти користувача
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password"));
+
+        // 2. Перевірити пароль (matches повертає true/false)
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("Invalid email or password");
+        }
+
+        // 3. Згенерувати JWT
+        return jwtService.generateToken(user.getEmail());
     }
 }
