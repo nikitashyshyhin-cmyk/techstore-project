818fcbb085006be83af518d96460b3e027e05e1b
timosha06 <248711184+timosha06@users.noreply.github.com>
2026-04-29 12:26:40 +0300

    feat(auth): create LoginRequest and update AuthResponse and update pom.xml

--- backend/src/main/java/com/techstore/dto/AuthResponse.java
+++ backend/src/main/java/com/techstore/dto/AuthResponse.java
@@ -3,12 +3,20 @@ package com.techstore.dto;
 public class AuthResponse {
 
     private String message;
+    private String token;
 
     public AuthResponse(String message) {
         this.message = message;
     }
 
+    public AuthResponse(String message, String token) {
+        this.message = message;
+        this.token = token;
+    }
+
+    // Геттери
     public String getMessage() {
         return message;
     }
+    public String getToken() {return token; }
 }
