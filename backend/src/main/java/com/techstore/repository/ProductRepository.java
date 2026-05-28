94bb000a1cc6705e3365aa0346454cc1faaa8a34
User <User@WIN-GH36CFH8229>
2026-05-29 00:08:32 +0300

    git commit -m "fix: bug fix"

--- backend/src/main/java/com/techstore/repository/ProductRepository.java
+++ backend/src/main/java/com/techstore/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.techstore.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
