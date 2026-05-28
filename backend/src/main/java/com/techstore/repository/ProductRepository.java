e58fd94b51d72daf3046135ba2d5835156ddc3c2
User <User@WIN-GH36CFH8229>
2026-05-28 22:01:36 +0300

    git commit -m "feat: add product filtering by category"

--- backend/src/main/java/com/techstore/repository/ProductRepository.java
+++ backend/src/main/java/com/techstore/repository/ProductRepository.java
@@ -4,13 +4,23 @@ import com.techstore.entity.Product;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByNameContainingIgnoreCase(
             String name,
+            Pageable pageable
+    );
+
+    Page<Product> findByCategories_Id(
+            Long categoryId,
+            Pageable pageable
+    );
+
+    Page<Product> findByNameContainingIgnoreCaseAndCategories_Id(
+            String name,
+            Long categoryId,
             Pageable pageable
     );
 }
