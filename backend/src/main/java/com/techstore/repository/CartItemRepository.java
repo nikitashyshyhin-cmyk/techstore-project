9e78eda8d20ee717da8a2de1b3ceab685c9ab269
timosha06 <248711184+timosha06@users.noreply.github.com>
2026-05-29 12:38:07 +0300

    feat(backend): Added method to fetch all cart items by user in CartItemRepository (US 5.2)

--- backend/src/main/java/com/techstore/repository/CartItemRepository.java
+++ backend/src/main/java/com/techstore/repository/CartItemRepository.java
@@ -1,16 +1,17 @@
 package com.techstore.repository;
 
 import com.techstore.entity.CartItem;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface CartItemRepository
-        extends JpaRepository<CartItem, Long> {
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     Optional<CartItem> findByUser_IdAndProduct_Id(
             Long userId,
             Long productId
     );
+
+    List<CartItem> findByUser_Id(Long userId);
 }
