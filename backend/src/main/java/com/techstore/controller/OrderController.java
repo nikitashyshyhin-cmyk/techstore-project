00070273f5c226adaa97c14348b42d67cc5523ac
timosha06 <248711184+timosha06@users.noreply.github.com>
2026-06-04 16:20:33 +0300

    feat(backend): implement OrderController, OrderSerivce and add security param in SecurityConfig

---
+++ backend/src/main/java/com/techstore/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.techstore.controller;
+
+import com.techstore.dto.CreateOrderResponse;
+import com.techstore.dto.OrderRequest;
+import com.techstore.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createOrder(@RequestBody OrderRequest request, Authentication authentication) {
+        try {
+            CreateOrderResponse response = orderService.createOrder(authentication.getName(), request);
+            return ResponseEntity.ok(response);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
